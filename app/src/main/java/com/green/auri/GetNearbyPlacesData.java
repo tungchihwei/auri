2ef32baa472675c71923c99dfef48d0701a86a9c
burstein <alanbur@bu.edu>
2018-11-27 10:39:15 -0500

    Added the v1 positioning logic

--- app/src/main/java/com/green/auri/GetNearbyPlacesData.java
+++ app/src/main/java/com/green/auri/GetNearbyPlacesData.java
@@ -1,5 +1,6 @@
 package com.green.auri;
 
+
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -7,18 +8,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {//implements GoogleMap.OnMarkerClickListener {
+public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {
     // a class that extends from async task
     // gets nearby place data and our case is when the user pressed the "Restaurant" button
     String googlePlacesData;
     GoogleMap mMap;
     String url;
+    double latitude;
+    double longitude;
+    final double SIGNIFICANCE_THRESHOLD = .001;
+
+    public GetNearbyPlacesData(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
 
     @Override
     protected String doInBackground(Object... params) {
@@ -43,7 +52,71 @@ public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {//im
         DataParser dataParser = new DataParser();
         nearbyPlacesList =  dataParser.parse(result); // parse json file and store in a HashMap
         ShowNearbyPlaces(nearbyPlacesList); // show these restuarants on the map
+        PositionNearbyPlaces(nearbyPlacesList);
         Log.d("GooglePlacesReadTask", "onPostExecute Exit");
+    }
+
+    private void PositionNearbyPlaces(List<HashMap<String, String>> nearbyPlacesList){
+        Log.i("Position", "My Position: "+latitude+" "+longitude);
+        for (int i = 0; i < nearbyPlacesList.size(); i++) {
+            HashMap<String, String> googlePlace = nearbyPlacesList.get(i);
+            double lat = Double.parseDouble(googlePlace.get("lat"));
+            double lng = Double.parseDouble(googlePlace.get("lng"));
+            String placeName = googlePlace.get("place_name");
+            Log.i("Position", "Place: "+placeName+" Position: "+lat+" "+lng);
+            double[] relativePositionList = RelativePosition(lat,lng);
+            Log.i("Position", relativePositionList[0]+ " " +relativePositionList[1]);
+        }
+    }
+
+    private double[] RelativePosition(double lat, double lng){
+        double latChange = latitude - lat;
+        double lngChange = longitude - lng;
+        Log.i("Position","latChange"+latChange);
+        Log.i("Position","lngChange"+lngChange);
+
+        double r = Math.sqrt(latChange*latChange + lngChange*lngChange);
+
+        double unitLat = latChange/r;
+        double unitLng = lngChange/r;
+
+        double theta = Math.toDegrees(Math.atan((lngChange)/(latChange)));
+
+//        Log.i("Position","theta: "+theta);
+//        Log.i("Position","distance: "+r);
+
+        Log.i("Position","unitLat: "+unitLat);
+        Log.i("Position","unitLng: "+unitLng);
+
+        double[] positions = {unitLat, unitLng};
+
+        return positions;
+
+//        if (latChange>SIGNIFICANCE_THRESHOLD && lngChange>SIGNIFICANCE_THRESHOLD){
+//            return "North East";
+//        }
+//        if (latChange>SIGNIFICANCE_THRESHOLD && lngChange<-SIGNIFICANCE_THRESHOLD){
+//            return "South East";
+//        }
+//        if (latChange<-SIGNIFICANCE_THRESHOLD && lngChange<-SIGNIFICANCE_THRESHOLD) {
+//            return "South West";
+//        }
+//        if (latChange<-SIGNIFICANCE_THRESHOLD && lngChange>SIGNIFICANCE_THRESHOLD){
+//            return "North West";
+//        }
+//        if (latChange>SIGNIFICANCE_THRESHOLD){
+//            return "East";
+//        }
+//        if (latChange<-SIGNIFICANCE_THRESHOLD){
+//            return "West";
+//        }
+//        if (lngChange>SIGNIFICANCE_THRESHOLD){
+//            return "North";
+//        }
+//        if (lngChange<-SIGNIFICANCE_THRESHOLD){
+//            return "South";
+//        }
+//        return "SAME";
     }
 
     private void ShowNearbyPlaces(List<HashMap<String, String>> nearbyPlacesList) {
@@ -56,31 +129,20 @@ public class GetNearbyPlacesData extends AsyncTask<Object, String, String> {//im
             double lng = Double.parseDouble(googlePlace.get("lng"));
             String placeName = googlePlace.get("place_name");
             String vicinity = googlePlace.get("vicinity");
-
-            String rating = googlePlace.get("rating");
-            Log.i("!!!","get rating");
-
             LatLng latLng = new LatLng(lat, lng);
             markerOptions.position(latLng);
-            markerOptions.title(placeName + ":" + vicinity + ":" + rating);
-
-
+            markerOptions.title(placeName + " : " + vicinity);
             mMap.addMarker(markerOptions);
-
-
-//            Card card = new Card();
-//            card.rname = markerOptions.getTitle();
 
             markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
             //move map camera
             mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            
-        }
-    }
 
-//    @Override
-//    public boolean onMarkerClick(Marker marker) {
-//        Log.i("!!!","marker");
-//        return false;
-//    }
+        }
+
+        // Position them
+        //Get current location
+        //Get direction you are facing
+        //Calculate degree offset from direction you are facing
+    }
 }
